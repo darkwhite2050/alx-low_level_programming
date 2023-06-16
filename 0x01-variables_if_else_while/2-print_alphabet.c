9c95823619fe83ba436a99ca3fc1b37386d0b4bb
Mekawi <ahmed.mekawwi@gmail.com>
2023-06-16 16:28:40 -0700

    m

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -15,10 +15,11 @@ int main(void)
 	char ch = 'a';
 
 	while (ch <= 'z')
-{
-	putchar(ch);
-	ch++;
-}
-putchar('\n');
-return (0);
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
 }
