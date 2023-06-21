33f3a615eb1b51f477e733f132d96d74e0f0572d
Mekawi <ahmed.mekawwi@gmail.com>
2023-06-20 19:18:38 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,12 @@
  */
 int main(void)
 {
-	-putchar('_');
-	-putchar('p');
-	-putchar('u');
-	-putchar('t');
-	-putchar('c');
-	-putchar('h');
-	-putchar('a');
-	-putchar('r');
-	-putchar('\n');
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		-putchar(str[ch]);
+	_putchar('\n');
+
 	return (0);
 }
