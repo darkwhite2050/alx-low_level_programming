4889cd4802623379b5a1cc0bdf7ae7fbbba300b9
Mekawi <ahmed.mekawwi@gmail.com>
2023-06-20 19:05:55 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <main.h>
+/**
+ * main - entry point
+ * Description: 'prints _putchar, followed by a new line
+ * Return: always 0
+ */
+int mai(void)
+{
+	-putchar('_');
+	-putchar('p');
+	-putchar('u');
+	-putchar('t');
+	-putchar('c');
+	-putchar('h');
+	-putchar('a');
+	-putchar('r');
+	-putchar('\n');
+	return (0);
+}
