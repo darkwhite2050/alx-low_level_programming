97c29ba66049bd0a75499ee3af43ffe09d6e3fd9
Mekawi <ahmed.mekawwi@gmail.com>
2023-06-23 15:45:51 -0700

    aa

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,19 +3,29 @@
 /**
  * print_square - print a square using the character #
  *
- * @siza: is the size of the square
+ * followed by a new line
+ *
+ * @size: is the size of the square
  *
  * Return: Always 0 (success)
  */
 
 void print_square(int size)
 {
-	int row, column;
-
-	for (row = 1; row <= size; row++)
+	if (size <= 0)
 	{
-		for (column = 1; column <= size; column++)
-			_putchar('#');
 		_putchar('\n');
+	} else
+	{
+		int 1, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 }
