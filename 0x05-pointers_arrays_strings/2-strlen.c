cc46da159fcbfe8cb0daf27f0b6ec14313f6f610
Mekawi <ahmed.mekawwi@gmail.com>
2023-06-26 12:36:48 -0700

    aa

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+i#include "main.h"
+
+/**
+ * _strlen - return the length of a string
+ *
+ * @s: string parameter input
+ *
+ * Return: Length of string
+ */
+
+int _strlen(char *s)
+{
+	int counter;
+
+	for (counter = 0; *s != '\0'; s++)
+		++counter;
+
+	return (counter);
+}
