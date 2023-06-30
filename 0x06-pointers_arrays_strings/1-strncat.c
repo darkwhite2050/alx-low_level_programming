63d96fc9fbd01e7fa19140082ad8fe1003ff0778
Mekawi <ahmed.mekawwi@gmail.com>
2023-06-30 11:17:46 -0700

    aa

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,20 +1,28 @@
-#include "bootcamp.h"
+#include "main.h"
 /**
- * _strncat - concatenates string with n bytes from another string
- * @dest: destination string
- * @src: source string
- * @n: bytes to be copied
+ *_strncat - function that concatenates two strings. it will use
+ * at most n bytes from src. src does not need to be null terminated.
+ *@dest: pointer to destination string.
+ *@src: pointer to source string.
+ *@n: number of bytes to be concatenated.
  *
- * Return: destination string concatenated
+ *Return: pointer to destination string.
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0, j;
+	int length, j;
+/* j is a counter for  n bytes of src to be concatenated */
+/* length = length of destination string */
 
-	while (dest[i] != '\0')
-		i++;
-	for (j = 0; j < n && src[j] != '\0'; j++, i++)
-		dest[i] = src[j];
-	dest[i] = '\0';
+	length = 0;
+	while (dest[length] != '\0')
+	{
+		length++;
+	}
+	for (j = 0; j < n && src[j] != '\0'; j++, length++)
+	{
+		dest[length] = src[j];
+	}
+	dest[length] = '\0';
 	return (dest);
-}
+}}
