0a63de54c12a762b437ade4cfff7f7ddb0cf922e
Mekawi <ahmed.mekawwi@gmail.com>
2023-07-10 13:54:55 -0700

    libmy

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,18 @@
+# include "main.h"
+
+/**
+ * _puts - prints a string, follwed by a newline to stdout
+ *
+ * @str: string parameter ot print
+ *
+ * Return: Nothing
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
