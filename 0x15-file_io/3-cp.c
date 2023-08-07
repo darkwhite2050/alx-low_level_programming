c4cfc98e3caed6f0283c6563d1b667494e0b862b
Mekawi <ahmed.mekawwi@gmail.com>
2023-08-07 11:46:19 -0700

    aa

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,7 +5,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-void check_io_status(int status, int fd, const char *filename, char mode);
+void check_IO_stat(int stat, int fd, char *filename, char mode);
 /**
  * main - copies the content of one file to another
  * @argc: argument count
@@ -15,61 +15,59 @@ void check_io_status(int status, int fd, const char *filename, char mode);
  */
 int main(int argc, char *argv[])
 {
-	int source, destination, bytes_read = 1024, bytes_written, close_source, close_destination;
-	unsigned int file_mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	int src, dest, n_read = 1024, wrote, close_src, close_dest;
+	unsigned int mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 	char buffer[1024];
 
-	if (argc != 3) {
-		fprintf(stderr, "Usage: cp file_from file_to\n");
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s", "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	source = open(argv[1], O_RDONLY);
-	check_io_status(source, -1, argv[1], 'O');
-
-	destination = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_mode);
-	check_io_status(destination, -1, argv[2], 'W');
-
-	while (bytes_read == 1024) {
-		bytes_read = read(source, buffer, sizeof(buffer));
-		if (bytes_read == -1) {
-			check_io_status(-1, -1, argv[1], 'O');
-		}
-
-		bytes_written = write(destination, buffer, bytes_read);
-		if (bytes_written == -1) {
-			check_io_status(-1, -1, argv[2], 'W');
-		}
+	src = open(argv[1], O_RDONLY);
+	check_IO_stat(src, -1, argv[1], 'O');
+	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+	check_IO_stat(dest, -1, argv[2], 'W');
+	while (n_read == 1024)
+	{
+		n_read = read(src, buffer, sizeof(buffer));
+		if (n_read == -1)
+			check_IO_stat(-1, -1, argv[1], 'O');
+		wrote = write(dest, buffer, n_read);
+		if (wrote == -1)
+			check_IO_stat(-1, -1, argv[2], 'W');
 	}
-
-	close_source = close(source);
-	check_io_status(close_source, source, NULL, 'C');
-
-	close_destination = close(destination);
-	check_io_status(close_destination, destination, NULL, 'C');
-
-	return 0;
+	close_src = close(src);
+	check_IO_stat(close_src, src, NULL, 'C');
+	close_dest = close(dest);
+	check_IO_stat(close_dest, dest, NULL, 'C');
+	return (0);
 }
 
 /**
- * check_io_status - checks if a file can be opened or closed
- * @status: file descriptor of the file to be opened
+ * check_IO_stat - checks if a file can be opened or closed
+ * @stat: file descriptor of the file to be opened
  * @filename: name of the file
  * @mode: closing or opening
  * @fd: file descriptor
  *
  * Return: void
  */
-void check_io_status(int status, int fd, const char *filename, char mode)
+void check_IO_stat(int stat, int fd, char *filename, char mode)
 {
-	if (mode == 'C' && status == -1) {
-		fprintf(stderr, "Error: Can't close fd %d\n", fd);
+	if (mode == 'C' && stat == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
-	} else if (mode == 'O' && status == -1) {
-		fprintf(stderr, "Error: Can't read from file %s\n", filename);
+	}
+	else if (mode == 'O' && stat == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
 		exit(98);
-	} else if (mode == 'W' && status == -1) {
-		fprintf(stderr, "Error: Can't write to %s\n", filename);
+	}
+	else if (mode == 'W' && stat == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
 		exit(99);
 	}
 }
